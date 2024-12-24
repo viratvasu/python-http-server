d846a6a8d23bb9b7811f4df5473cb95b3fd028e0
vasuvirat <haribodicharla2233@gmail.com>
2024-12-24 21:01:55 +0530

    http response with starline

--- app/main.py
+++ app/main.py
@@ -2,13 +2,19 @@ import socket  # noqa: F401
 
 
 def main():
-    # # You can use print statements as follows for debugging, they'll be visible when running tests.
-    # print("Logs from your program will appear here!")
-
-    # # Uncomment this to pass the first stage
-    # #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    while True:
+      client_socket, client_address = server_socket.accept() # wait for client
+      print(f"Connection received from {client_address}")
+      # Read the request from the client
+      request = client_socket.recv(1024).decode('utf-8')
+      print(f"Request: {request}")
+      if request.startswith("GET"):
+          response = "HTTP/1.1 200 OK\r\n\r\n"
+          client_socket.sendall(response.encode('utf-8'))
+      # return the response with start-line
+      #<protocol> <status-code> <status-text>
+      client_socket.close()
 
 
 if __name__ == "__main__":
