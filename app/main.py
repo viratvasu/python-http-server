28b78931714148a929984bf4e1ae0fc6e75eb066
vasuvirat <haribodicharla2233@gmail.com>
2024-12-24 23:18:07 +0530

    retry

--- app/main.py
+++ app/main.py
@@ -2,35 +2,28 @@ import threading
 import socket  # noqa: F401
 
 
-def handle_request(socket_conn, addr):
-    try:
-        # Read the request from the client
-        request = socket_conn.recv(4096).decode("utf-8")
-        print(f"Received request from {addr}:\n{request}")
-
-        # Respond with the required HTTP response
-        response = b"HTTP/1.1 200 OK\r\n\r\n"
-        socket_conn.sendall(response)
-    except Exception as e:
-        print(f"Error handling request from {addr}: {e}")
-    finally:
-        # Close the connection
-        socket_conn.close()
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    print("Server is listening on port 4221...")
-
     while True:
-        try:
-            # Accept a new connection
-            client_socket, client_address = server_socket.accept()
-            print(f"Connection received from {client_address}")
-
-            # Handle each client in a separate thread
-            threading.Thread(target=handle_request, args=(client_socket, client_address)).start()
-        except Exception as e:
-            print(f"Error accepting connection: {e}")
+      client_socket, client_address = server_socket.accept() # wait for client
+      print(f"Connection received from {client_address}")
+      # Read the request from the client
+      request = client_socket.recv(4096).decode("utf-8")
+      client_data = request.split(" ")
+      target = client_data[1]
+      if target == "/":
+          response = b"HTTP/1.1 200 OK\r\n\r\n"
+      elif target.startswith("/echo/"):
+          value = target.split("/echo/")[1]
+          response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(value)}\r\n\r\n{value}".encode()
+      elif target.startswith("/user-agent"):
+          value=client_data[-1].strip()
+          response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(value)}\r\n\r\n{value}".encode()
+          # response = b"HTTP/1.1 200 OK\r\n\r\n"
+      else:
+          response=b"HTTP/1.1 404 Not Found\r\n\r\n"
       
+      client_socket.sendall(response)
 
 
 if __name__ == "__main__":
