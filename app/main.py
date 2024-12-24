3a141a7a5b76c3d2973343e5bb63de9465c71327
vasuvirat <haribodicharla2233@gmail.com>
2024-12-24 23:51:04 +0530

    retry

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import threading
 import socket  # noqa: F401
+import sys
 
 
 def handle_request(client_socket, client_address):
@@ -17,6 +18,15 @@ def handle_request(client_socket, client_address):
       value=client_data[-1].strip()
       response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(value)}\r\n\r\n{value}".encode()
       # response = b"HTTP/1.1 200 OK\r\n\r\n"
+  elif target.startswith("/files"):
+      directory = sys.argv[2]
+      filename = target[7:]
+      try:
+          with open(f"/{directory}/{filename}", "r") as f:
+              body = f.read()
+          response = f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {len(body)}\r\n\r\n{body}".encode()
+      except Exception as e:
+          response = b"HTTP/1.1 404 Not Found\r\n\r\n"
   else:
       response=b"HTTP/1.1 404 Not Found\r\n\r\n"
   
