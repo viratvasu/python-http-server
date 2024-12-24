fef4611e47928592e4b59fb27ba828e4b7530333
vasuvirat <haribodicharla2233@gmail.com>
2024-12-24 21:39:12 +0530

    sending response based on path parameters

--- app/main.py
+++ app/main.py
@@ -3,18 +3,15 @@ import socket  # noqa: F401
 
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    while True:
-      client_socket, client_address = server_socket.accept() # wait for client
-      print(f"Connection received from {client_address}")
-      # Read the request from the client
-      request = client_socket.recv(1024).decode('utf-8')
-      print(f"Request: {request}")
-      if request.startswith("GET"):
-          response = "HTTP/1.1 200 OK\r\n\r\n"
-          client_socket.sendall(response.encode('utf-8'))
-      # return the response with start-line
-      #<protocol> <status-code> <status-text>
-      client_socket.close()
+    client_socket, client_address = server_socket.accept() # wait for client
+    print(f"Connection received from {client_address}")
+    # Read the request from the client
+    request = client_socket.recv(4096).decode("utf-8")
+    client_data = request.split(" ")
+    if client_data[1] == "/":
+        client_socket.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    else:
+        client_socket.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
 
 
 if __name__ == "__main__":
